08aa65947aff0422825245b7519c60d230ab9d82
Cody Breunig <breunigc@oregonstate.edu>
2026-01-19 18:46:30 -0800

    completed assignment 1

--- prog1.c
+++ prog1.c
@@ -1,11 +1,25 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
+#include <stdlib.h>
+#include <time.h>
 
 
 int main() {
 	// The main function is the entrypoint of the program. When the program
 	// is executed, it will automatically start here.
+	srand(time(NULL));
+	int myrand = (rand() % 11) + 0; //syntax is (rand() % max-min+1)+min
 	
-	// TODO Complete the program
+
+	if (myrand >= 0 && myrand <= 4) {
+		printf("Eat more beef, kick less cats \n");
+	} 
+	else if (myrand >=5 && myrand <= 9) {
+		printf("FRODO LIVES \n");
+	}
+	else {
+		printf("larn is the best rouguelike \n");
+	}
+	
+	printf("the random number was %d\n", myrand);
+
 }
